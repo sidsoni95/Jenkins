34f0a1c4c4599afda5138faae98782d482fc0584
Siddharth Soni <sidson.sasss2007@gmail.com>
2020-09-08 20:42:00 +0530

    4th commit

--- src/main/java/com/sid/training/jenkins/JenkinsCalculator.java
+++ src/main/java/com/sid/training/jenkins/JenkinsCalculator.java
@@ -4,7 +4,6 @@ public class JenkinsCalculator {
 	
 	//Method to add 2 numbers
 	public int addNumbers(int numberOne, int numberTwo) {
-		int var1 = 20;
 		return numberOne + numberTwo;
 	}
 	//Method to subtract 2 numbers
